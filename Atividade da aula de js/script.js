879be2a7ad4e85207d9321d9c83eb3cdf0c808a1
JoseNiltonDev <jasfagundesmusic@gmail.com>
2025-01-22 16:51:26 -0300

    Atividade finalizada

--- Atividade da aula de js/script.js
+++ Atividade da aula de js/script.js
@@ -3,3 +3,22 @@
 const titulo = document.getElementById("titulo");
 
 titulo.innerText = 'Título principal';
+
+const elementoA = document.getElementById('a');
+elementoA.innerText = 'Proz - educação';
+
+const elementoUL = document.getElementById('lista-nao-ordenada');
+elementoUL.innerHTML = `
+    <li> Elemento 1 </li>
+    <li> Elemento 2 </li>
+    <li> Elemento 3 </li>
+`;
+
+const elementoOL = document.getElementById('lista-ordenada');
+elementoOL.innerHTML = `
+    <li><a href="https://www.globo.com/">GLOBO</li>
+    <li><a href="https://www.youtube.com/watch?v=OYPbr6ZG3pc">Youtube Curso de JS</li>
+    <li><a href="https://www.instagram.com/?hl=en">Instagram</li>
+`
+
+
