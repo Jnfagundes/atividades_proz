6919d87bf266ad65fcf90bf8b1330c8f43bc2309
JoseNiltonDev <jasfagundesmusic@gmail.com>
2025-01-22 16:33:33 -0300

    Atividade da proz

---
+++ Atividade da aula de js/script.js
@@ -0,0 +1,5 @@
+//capture os quatro elementos criados, e use a propriedade .innerText para adicionar conteúdo textual aos elementos 'h1' e 'a', e a propriedade .innerHTML para adicionar três itens simples na lista não ordenada, e três itens com links para outros sites na lista ordenada.
+
+const titulo = document.getElementById("titulo");
+
+titulo.innerText = 'Título principal';
