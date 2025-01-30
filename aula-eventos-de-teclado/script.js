48784ee09daaf9298c3ee7579f8418535e46e8ec
JoseNiltonDev <jasfagundesmusic@gmail.com>
2025-01-30 20:27:51 -0300

    Adicionando eventos do mouse e teclado

--- aula-eventos-de-teclado/script.js
+++ aula-eventos-de-teclado/script.js
@@ -4,3 +4,11 @@ const navPerfil = document.getElementById("nav-perfil");
 linkPerfil.addEventListener("mouseover", ()=> {
   navPerfil.style.display = "block"
 })
+
+linkPerfil.addEventListener("mouseleave", ()=> {
+  navPerfil.style.display = "none";
+});
+
+window.addEventListener("keydown", (e) => {
+  console.log(e.key);
+});
